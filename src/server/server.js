29719be2aa87253aa0ce1fcefd67841bc521aa1c
Ron Crisostomo <rcrisostomo97@gmail.com>
2020-01-10 16:34:16 +1300

    Connection establishment with MongoDB database

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,16 @@
 require('dotenv/config');
+const mongoose = require("mongoose");
+
+// Server setup
 let io = require("socket.io")();
 io.listen(process.env.SERVER_PORT);
 console.log("Server listening on port number: ", process.env.SERVER_PORT);
+
+// Establishing connection with mongo db server
+mongoose.connect(process.env.MONGO_DB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connection.once("open", () => {
+    console.log("Succesfully established a connection with MongoDB database");
+});
 
 io.on("connection", clientSocket => {
     console.log("Client connection detected: ", clientSocket.id);
