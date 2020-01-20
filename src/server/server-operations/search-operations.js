d72e2de74afe4172ec9bdb8d458816a078f6745b
Ron Crisostomo <rcrisostomo97@gmail.com>
2020-01-20 18:01:33 +1300

    Removed searchGroups method to adhere to DRY. Converted searchFriends into a single search method that will work for group or friend queries

--- src/server/server-operations/search-operations.js
+++ src/server/server-operations/search-operations.js
@@ -1,45 +1,36 @@
 import SEARCH_EVENTS from "../../events/search-events";
 class SearchOperations{
 
-    static searchFriends(clientSocket, dbConnection, data){
+    /**
+     * Performs a search through the database based on some string query
+     * @param clientSocket {Socket} - Socket object of the client that requested the search
+     * @param dbConnection {DatabaseConnection} - DatabaseConnection object to perform the query with the databse
+     * @param data {Object} - Object passed through socket io events. In this case, it should contain stringQuery which is the value to be searched for in the collections 
+     * @param mode {String} - Indicates whether we are searching for "Friends" or "Groups"
+    */
+    static search(clientSocket, dbConnection, data, mode){
 
         // Notice the usage of RegExp - This is so we can do a substring search when looking for the docs in the database
-        // (Similar to doing LIKE data.stringQuery% in SQL but case isensitive)
-        const query = {userName: new RegExp(data.stringQuery, 'i')}; 
+        // (Similar to doing LIKE data.stringQuery% in SQL but case insensitive)
+        const query = mode === "Friends" ? {userName: new RegExp(data.stringQuery, 'i')} : {groupName: new RegExp(data.stringQuery, 'i')}; 
 
         // TODO: Need to filter out clientSocket's user name from the results
-        dbConnection.findDocumentsInCollection("users", query, (results) => {
+        dbConnection.findDocumentsInCollection(mode === "Friends" ? "users" : "groups", query, (results) => {
             if(results.length === 0){
                 console.log("No results found as a result of the query for SEARCH_EVENTS.SEARCH_FRIENDS");
                 clientSocket.emit(SEARCH_EVENTS.NO_RESULTS_FOUND);
                 return;
             }
 
-            const resultingUserNames = [];
+            const resultingNames = [];
             results.map((value, index) => {
-                resultingUserNames.push(value.userName);
+                resultingNames.push(value.userName);
             });
 
-            clientSocket.emit(SEARCH_EVENTS.DELIVER_RESULTS, {results: resultingUserNames});
+            clientSocket.emit(SEARCH_EVENTS.DELIVER_RESULTS, {results: resultingNames});
         });
     }
 
-    static searchGroups(clientSocket, dbConnection, data){
-        const query = {groupName: data.stringQuery};
-        dbConnection.findDocumentsInCollection("groups", query, (results) => {
-            if(results.length === 0){
-                // TODO: Emit SEARCH_EVENTS.NO_RESULTS_FOUND;
-                console.log("No results found as a result of the query for SEARCH_EVENTS.SEARCH_GROUPS");
-                return;
-            }
-
-            // TODO: Emit userNames back to client side?
-            console.log(`Results found for query: ${data.stringQuery} for SEARCH_EVENTS.SEARCH_GROUPS`);
-            for(let result in results){
-                console.log(result.groupName);
-            }
-        });
-    }
 }
 
 module.exports = SearchOperations;
