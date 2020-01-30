95142a4a2dae4f4b53dbead90d6b0767bf209235
Ron Crisostomo <rcrisostomo97@gmail.com>
2020-01-30 19:05:58 +1300

    Began styling CreateGroupDisplayView

--- src/client/components/displayable-views/CreateGroupDisplayView.jsx
+++ src/client/components/displayable-views/CreateGroupDisplayView.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import "./displayable-views-css-files/create-group-displayble-view-styles.css";
 export default function CreateGroupDisplayView(props){
     return(
         <div id="createGroupDisplayView">
-            <h1>CreateGroupDisplayeView</h1>
+            <div id="createGroupDisplayViewContent">
+                <h1>Create a Group</h1>
+                <div id="groupDetailsInput">
+                    <h2 className="groupDetailsInputLabel">Group Name</h2>
+                    <input className="groupDetailsInputField" type="text"></input>
+
+                    <h2 className="groupDetailsInputLabel">Group Description</h2>
+                    <input className="groupDetailsInputField" type="text"></input>
+                </div>
+            </div>
         </div>
     )
 }
