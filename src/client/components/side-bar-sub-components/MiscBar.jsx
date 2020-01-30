6d5335ecc08ba503cae4003988a15386ff8711f6
Ron Crisostomo <rcrisostomo97@gmail.com>
2020-01-30 14:38:26 +1300

    Updated MiscBar to use context-menu based navigation

--- src/client/components/side-bar-sub-components/MiscBar.jsx
+++ src/client/components/side-bar-sub-components/MiscBar.jsx
@@ -2,20 +2,25 @@ import React from "react";
 import "./side-bar-sub-components-css-files/misc-bar-styles.css"
 import socket from "../../../index";
 import USER_ACTION_EVENTS from "../../../events/user-action-events";
+import { ContextMenu, MenuItem, ContextMenuTrigger } from "react-contextmenu";
 export default function MiscBar(props){
-    const backSign = "<";
-    const forwardSign = ">";
 
-    const handleSettingsClicked = () => {
+    const handleClick = () => {
         // TODO: Clicking settings will create a group - THIS IS TEMPORARY!
-        socket.emit(USER_ACTION_EVENTS.CREATE_GROUP, {groupName: "TestGroup"});
+        // socket.emit(USER_ACTION_EVENTS.CREATE_GROUP, {groupName: "TestGroup"});
     }
 
     return(
         <div id="miscBar">
-            <button>{backSign}</button>
-            <button>{forwardSign}</button>
-            <button onClick={handleSettingsClicked}>Settings</button>
+            <ContextMenuTrigger id="settingsContextMenu" holdToDisplay={1}> {/*holdToDisplay prop makes it so that you can left click to show the context menu*/}
+                <div id="settingsDiv">Settings</div>
+            </ContextMenuTrigger>
+
+            <ContextMenu id="settingsContextMenu">
+                <MenuItem className="miscBarMenuItem">Create Group</MenuItem>
+                <MenuItem className="miscBarMenuItem">Logout</MenuItem>
+            </ContextMenu>
+
         </div>
     );
 }
