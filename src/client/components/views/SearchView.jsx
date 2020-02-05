4f09aab1d2a187b2696c5b75c5ebe266fa87461d
Ron Crisostomo <rcrisostomo97@gmail.com>
2020-02-05 17:20:48 +1300

    Added SearchBar components to SearchView

--- src/client/components/views/SearchView.jsx
+++ src/client/components/views/SearchView.jsx
@@ -1,12 +1,31 @@
 import React from "react"
 import "./views-css-files/search-view-styles.css";
+import SearchBar from "./views-sub-components/SearchBar";
 export default function SearchView(props){
+
+    const updateUserCommEntities = (userCommEntities) => {
+
+    }
+
+    const updateGroupCommEntities = (groupCommEntities) => {
+
+    }
+
+    const resetUserCommEntities = () => {
+
+    }
+
+    const resetGroupCommEntities = () => {
+
+    }
+
     return(
         <div id="searchViewWrapper">
             <div id="userSearchWrapper">
                 <div id="userSearchContent" className="content">
                     <div id="userSearchBar" className="searchBarDiv">
-
+                        <h2>Users</h2>
+                        <SearchBar mode="Friends" updateCommEntities={updateUserCommEntities} resetCommEntities={resetUserCommEntities}/>
                     </div>
                     <div id="userSearchResults" className="resultsDiv">
 
@@ -17,6 +36,8 @@ export default function SearchView(props){
             <div id="groupSearchWrapper">
                 <div id="groupSearchContent" className="content">
                     <div id="groupSearchBar" className="searchBarDiv">
+                        <h2>Groups</h2>
+                        <SearchBar mode="Groups" updateCommEntities={updateGroupCommEntities} resetCommEntities={resetGroupCommEntities}/>
 
                     </div>
                     <div id="groupSearchResults" className="resultsDiv">
