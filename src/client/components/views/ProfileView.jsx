f09340093a202a573612177d7fbb942e73dabd09
Ron Crisostomo <rcrisostomo97@gmail.com>
2020-02-07 15:57:29 +1300

    Began laying out ProfileView

--- src/client/components/views/ProfileView.jsx
+++ src/client/components/views/ProfileView.jsx
@@ -1,8 +1,21 @@
-import React from "react"
-export default function ProfileDisplayView(props){
+import React from "react";
+import "./views-css-files/profile-view-styles.css";
+export default function ProfileView(props){
     return(
-        <div id="profileDisplayView">
-            <h1>ProfileDisplayView</h1>
-        </div>
+        <div id="profileViewWrapper">
+            <div id="bannerDiv">
+                <div id="profileImageDiv"></div>
+                <div id="titleInfo"></div>
+            </div>
+
+            <div id="profileBodyWrapper">
+                <div id="friendsAndGroupsWrapper">
+                    <div id="friendsDiv"></div>
+                    <div id="groupsDiv"></div>
+                </div>
+
+                <div id="accountInformationWrapper"></div>
+                </div>
+            </div>
     )
 }
